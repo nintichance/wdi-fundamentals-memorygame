3fc6744f1d047dc3f83114ee245563526b016375
Ninti Chance <nintichance@Nintis-MacBook-Pro.local>
2017-11-12 10:44:35 -0500

    created game logic

--- main.js
+++ main.js
@@ -1,26 +1,22 @@
 console.log("Up and running!");
 var cards = ["queen","queen","king","king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User flipped queen");
-var cardThree = cards[3];
-cardsInPlay.push(cardThree);
-console.log("User flipped king");
-var cardFour = cards[2];
-cardsInPlay.push(cardFour);
-console.log("User flipped king");
-console.log(cardsInPlay);
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You have a match!");
-} else {
-	alert("Sorry, try again.");
+
+var checkForMatch = function(){
+if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	}else{
+		alert("Sorry, try again.");
+	}
 }
 
+var flipCard = function(cardID) {
+	console.log("User flipped " + cards[cardID])
+	cardsInPlay.push(cards[cardID])
+	checkForMatch(0)
+}
 
+flipCard(0)
+flipCard(2)
 
-
-
+	
