348e8bcef59d8dd8b89c09b921f687d0711ad2a2
Ninti Chance <nintichance@Nintis-MacBook-Pro.local>
2017-11-11 22:21:47 -0500

    created gamed logic

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour); 
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log("User flipped queen");
+var cardThree = cards[3];
+cardsInPlay.push(cardThree);
+console.log("User flipped king");
+var cardFour = cards[2];
+cardsInPlay.push(cardFour);
+console.log("User flipped king");
+console.log(cardsInPlay);
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You have a match!");
+} else {
+	alert("Sorry, try again.");
+}
+
+
+
+
+
